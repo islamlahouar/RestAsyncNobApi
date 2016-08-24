feeb6822d4913572dd46cbb000276d8db5f233ea
islamlahouar <islam.lahouar@gmail.com>
2016-08-24 17:19:28 +0200

    premier commit : push du skeleton

---
+++ src/main/java/com/first/RestAsyncNobApiApplication.java
@@ -0,0 +1,12 @@
+package com.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAsyncNobApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAsyncNobApiApplication.class, args);
+	}
+}
